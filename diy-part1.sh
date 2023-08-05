8a22aa5418dac41938941e3f282cae8130a713c2
iouyjl <93931701+iouyjl@users.noreply.github.com>
2023-08-06 00:03:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
